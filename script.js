4e0f4ee62c67cc5263cd9a54b708289ab5e410c7
SynthCyraX <SynthCyraX@gmail.com>
2023-06-24 14:01:35 +0200

    Changed from grid to table

--- script.js
+++ script.js
@@ -18,27 +18,18 @@ function addBookToLibrary(title, author, pages, read){
     return myLibrary.push(bookTitle)
 }
 
-let content = document.getElementById("content")
-
-
-// Finde das HTML-Element, in dem der Inhalt angezeigt werden soll
 let contentElement = document.getElementById("content");
 
-// Durchlaufe das Array und generiere HTML-Elemente für jedes Objekt
 for (let i = 0; i < myLibrary.length; i++) {
   let item = myLibrary[i];
 
-  // Erstelle ein <div> Element für das aktuelle Objekt
-  let itemElement = document.createElement("div");
+  let row = contentElement.insertRow();
+  var nameCell = row.insertCell();
+  nameCell.textContent = item.title;
 
-  // Setze den Textinhalt des <div> Elements basierend auf den Objektdaten
-  itemElement.textContent = "Title: " + item.title + ", Author: " + item.author + ", Pages: " + item.pages + ", read: " + item.read;
+  var authorCell = row.insertCell();
+  authorCell.textContent = item.author;
 
-  // Füge das <div> Element dem Hauptelement hinzu
-  contentElement.appendChild(itemElement);
+  var pagesCell = row.insertCell();
+  pagesCell.textContent = item.pages;
 }
-            // const array = [1, 2, 3, 4, 5];
-
-            // const string = JSON.stringify(myLibrary, null, 4);
-
-            // document.querySelector(".output").innerHTML = string;
