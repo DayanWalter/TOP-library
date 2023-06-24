d32bfb6bd9a4e9e426ea216dca36bb6618aabac3
SynthCyraX <SynthCyraX@gmail.com>
2023-06-24 13:49:34 +0200

    First draft loop through the objects

--- script.js
+++ script.js
@@ -13,9 +13,32 @@ function Book(title, author, pages, read) {
   }
 }
 
-
-
 function addBookToLibrary(title, author, pages, read){
     const bookTitle = new Book(title, author, pages, read)
     return myLibrary.push(bookTitle)
 }
+
+let content = document.getElementById("content")
+
+
+// Finde das HTML-Element, in dem der Inhalt angezeigt werden soll
+let contentElement = document.getElementById("content");
+
+// Durchlaufe das Array und generiere HTML-Elemente für jedes Objekt
+for (let i = 0; i < myLibrary.length; i++) {
+  let item = myLibrary[i];
+
+  // Erstelle ein <div> Element für das aktuelle Objekt
+  let itemElement = document.createElement("div");
+
+  // Setze den Textinhalt des <div> Elements basierend auf den Objektdaten
+  itemElement.textContent = "Title: " + item.title + ", Author: " + item.author + ", Pages: " + item.pages + ", read: " + item.read;
+
+  // Füge das <div> Element dem Hauptelement hinzu
+  contentElement.appendChild(itemElement);
+}
+            // const array = [1, 2, 3, 4, 5];
+
+            // const string = JSON.stringify(myLibrary, null, 4);
+
+            // document.querySelector(".output").innerHTML = string;
