bc033b89671a14c7a65e8584ebe56181a3702c5e
SynthCyraX <SynthCyraX@gmail.com>
2023-06-27 20:37:19 +0200

    Changed structure/order from script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let myLibrary = [lotr];
 
 // function for adding a Book-object to the library-array
 function addBookToLibrary(){
-  // get values from the Add-Form input fields
+  // get values from the addForm input fields
   let title = document.getElementById('title').value;
   let author = document.getElementById('author').value;
   let pages = document.getElementById('pages').value;
@@ -26,34 +26,44 @@ function addBookToLibrary(){
 
   // create new Book-object and...
   const newBook = new Book(title, author, pages, read)
+
   // ...add to library...
   myLibrary.push(newBook)
+
   // ...and refresh immediatly:
   refreshTable();
 }
+
 function updateIndex() {
   myLibrary.forEach((book, index) => {
     book.index = index;
   });
 }
+
 // after clicking addBookButton
 addForm.addEventListener('submit', function(event) {
+
   // prevent default behaviour after clicking addBookButton(submit-button)
   event.preventDefault(); 
+
   // invoke function for adding a new book
   addBookToLibrary();
 });
 
 let arrayTable = document.getElementById("array-table");
+
 // function to refresh the table
 function refreshTable(){
+
     // delete everything in the table, except the header(index 0)
     while (arrayTable.rows.length > 1) {
       arrayTable.deleteRow(1);
     }
+
     // after deletion, update the index of every object
     updateIndex();
-    // add every object in the array to the table
+
+    // add every object from the array into the table
     for (let i = 0; i < myLibrary.length; i++) {
     let book = myLibrary[i];
 
@@ -75,10 +85,10 @@ function refreshTable(){
     indexCell.textContent = book.index;
   }
 }
+
 // refreshButton wired with function
 let refreshButton = document.getElementById("refreshButton")
 refreshButton.addEventListener("click", refreshTable)
-
 
 // toggle-visibility
 let newBookButton = document.getElementById("newBookButton");
@@ -97,18 +107,22 @@ newBookButton.addEventListener("click", () => {
   }
 })
 
-// delete book
+// delete book via index from array and table
 function deleteBookFromLibrary(){
   let index = document.getElementById('deleteIndex').value;
 
   myLibrary.splice(index, 1)
 }
+
 // after clicking deleteButton(submit)
 deleteForm.addEventListener('submit', function(event) {
+
   // prevent default behaviour
   event.preventDefault();
+
   // invoke function for deleting book at index x
   deleteBookFromLibrary();
+
   // refresh the index
   refreshTable();
 });
@@ -126,10 +140,10 @@ deleteBookButton.addEventListener("click", () => {
     form.style.display = "none";
   }
 })
+
 // resetButton for addForm
 let resetButton = document.getElementById("resetButton");
 resetButton.addEventListener("click", () => {
   document.getElementById('addForm').reset();
-
 })
 
